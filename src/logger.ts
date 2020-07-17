d364ec877944e52d1bedfab55fdb9c7eb1a93ac6
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-17 04:16:32 -0300

    Re-added format error: stack

--- src/logger.ts
+++ src/logger.ts
@@ -4,6 +4,7 @@ import { info } from "console";
 export const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
     format.colorize(),
     format.align(),
     format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
