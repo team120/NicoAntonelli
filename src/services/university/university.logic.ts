12ec662329ee405c8ca504134eeb4eb31ef7888d
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-17 17:11:13 -0300

    Add saveUniversityLogicFactory

--- src/services/university/university.logic.ts
+++ src/services/university/university.logic.ts
@@ -6,16 +6,23 @@ import { University } from "../../entities/university/university.model";
 export const getUniversitiesLogicFactory = (
   getQuery: queryTypes.getQueryFunc,
 ) => (): Promise<UniversityShowDto[]> =>
-  getQuery(University).then((universities) =>
-    universities.map((university) => plainToClass(UniversityShowDto, university)),
-  ); // Add Departments...
+    getQuery(University).then((universities) =>
+      universities.map((university) => plainToClass(UniversityShowDto, university)),
+    );
 
 export const getOneUniversityLogicFactory = (
   getOneQuery: queryTypes.getOneQueryFunc,
 ) => (id: number): Promise<UniversityShowDto> =>
-  getOneQuery(University, id).then((university) =>
+    getOneQuery(University, id).then((university) =>
+      plainToClass(UniversityShowDto, university),
+    );
+
+export const saveUniversityLogicFactory = (
+  getSaveQuery: queryTypes.saveQueryFunc,
+) => (university: University) => getSaveQuery(University, university)
+  .then((university) =>
     plainToClass(UniversityShowDto, university),
-  ); // Add Departments...
+  );
 
 export const deleteUniversityLogicFactory = (
   deleteQuery: queryTypes.deleteQueryFunc,
