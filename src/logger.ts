216ae3c639c1756366c8257ea09293467e48f65b
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-17 03:31:39 -0300

    Logger first changes

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,18 @@
 import { createLogger, transports, format } from "winston";
+import { info } from "console";
 
 export const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
+    format.colorize(),
+    format.align(),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
-  transports: [new transports.Console()],
+  transports: [
+    new (transports.Console)({
+      handleExceptions: true,
+      level: "info"
+    })
+  ],
   exitOnError: false,
 });
