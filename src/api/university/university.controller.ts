b3f8f423d803e6b5559ca9e7e109f42864a87b1a
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-17 18:35:18 -0300

    Create, Update, Delete University (First Approach) --> Some Bugs

--- src/api/university/university.controller.ts
+++ src/api/university/university.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import * as universityServices from "../../services/university/university.logic.setup";
+import { getRepository } from "typeorm";
+import { University } from "src/entities/university/university.model";
 
+// Get All
 export const getAllUniversities = (
   req: Request,
   res: Response,
@@ -14,6 +17,7 @@ export const getAllUniversities = (
     .catch((err) => next(err));
 };
 
+// Get One
 export const getOneUniversity = (
   req: Request,
   res: Response,
@@ -23,6 +27,50 @@ export const getOneUniversity = (
     .getOneUniversity(Number(req.params.id))
     .then((university) => {
       res.status(200).json(university);
+    })
+    .catch((err) => next(err));
+};
+
+// // Create --> To Fix
+// export const createUniversity = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction): void => {
+//     universityServices
+//     .saveUniversity(getRepository(University).create(req.body as University))
+//     .then((university) => {
+//       res.status(200).json(university);
+//     })
+//     .catch((err) => next(err));
+// };
+
+// Update --> To Fix
+// export const updateUniversity = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction): void => {
+//     const university = await getRepository(University).findOne(Number(req.params.id))
+//     .then((university) => {
+//       if (university !=== undefined) {
+//         universityServices
+//         .saveUniversity(university)
+//         .then((university) => {
+//           res.status(200).json(university);
+//         })
+//         .catch((err) => next(err));
+//       }
+//       .catch((err) => next(err));
+//     })
+// };
+
+// Delete
+export const deleteUniversity = (
+  req: Request,
+  res: Response,
+  next: NextFunction): void => {
+    universityServices.deleteUniversity(Number(req.params.id))
+    .then(() => {
+      res.status(200);
     })
     .catch((err) => next(err));
 };
--- src/api/university/university.route.ts
+++ src/api/university/university.route.ts
@@ -5,5 +5,8 @@ const universityRouter = Router();
 
 universityRouter.get("", universityActions.getAllUniversities);
 universityRouter.get("/:id", universityActions.getOneUniversity);
+// universityRouter.post('/api/users', universityActions.createUniversity);
+// universityRouter.put('/api/users/:id', updateUniversity);
+universityRouter.delete('/api/users/:id', universityActions.deleteUniversity);
 
 export default universityRouter;
