a8b19e97b954eb8fe5833e59ceb254e17e62cada
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-07-19 17:21:00 -0300

    Add google credentials to env file and reflect that in config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,9 @@ dotenv.config();
 export const env = {
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
+  googleAuth: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackUrl: "http://localhost:3000/auth/google/callback",
+  },
 };
