bab61e59cf52a1e3e6f50127252ea4ae08158b79
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-07-19 20:48:51 -0300

    Add social login routes

--- src/api/auth/auth.route.ts
+++ src/api/auth/auth.route.ts
@@ -4,6 +4,7 @@ import { RegisterInputDto } from "../../entities/auth/input/register.input.dto";
 import { sanitizeWith } from "../../middlewares/sanitization.middleware";
 import { validateWith } from "../../middlewares/validation.middleware";
 import { LoginInputDto } from "../../entities/auth/input/login.input.dto";
+import passport from "passport";
 
 const authRouter = Router();
 
@@ -17,6 +18,16 @@ authRouter.post(
   "/login",
   [sanitizeWith(LoginInputDto), validateWith(LoginInputDto, "body")],
   authActions.login,
+);
+
+authRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { failureMessage: "google failed" }),
 );
 
 export default authRouter;
