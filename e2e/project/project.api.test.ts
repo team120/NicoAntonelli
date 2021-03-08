bdb9943f3b0729a56d024def53c22bac72cb3b53
ale <alejandrorecalde5@gmail.com>
2021-03-08 20:20:35 -0300

    Add test cases when query string parameters are provided

--- e2e/project/project.api.test.ts
+++ e2e/project/project.api.test.ts
@@ -43,4 +43,114 @@ describe("Project actions", () => {
         });
     });
   });
+
+  describe("search projects", () => {
+    it("should get all projects that partially match their name", async () => {
+      const name = "Manager";
+      await request(api)
+        .get(`/projects?name=${name}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.body[0]).toEqual({
+            name: "University Project Manager",
+            type: "Informal",
+            isDown: false,
+            department: {
+              name: "Ingenieria en Sistemas",
+            },
+            users: [
+              {
+                mail: "user3@example.com",
+                lastName: null,
+                name: "user3",
+                university: {
+                  name: "UTN",
+                },
+              },
+              {
+                mail: "user2@example.com",
+                name: "user2",
+                lastName: null,
+                university: {
+                  name: "UTN",
+                },
+              },
+            ],
+          });
+          expect(res.body).toHaveLength(1);
+        });
+    });
+    it("should get all projects that partially match some of their users", async () => {
+      const fullNamePartial = "User3";
+      await request(api)
+        .get(`/projects?user=${fullNamePartial}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.body[0]).toEqual({
+            name: "University Project Manager",
+            type: "Informal",
+            isDown: false,
+            department: {
+              name: "Ingenieria en Sistemas",
+            },
+            users: [
+              {
+                mail: "user3@example.com",
+                lastName: null,
+                name: "user3",
+                university: {
+                  name: "UTN",
+                },
+              },
+              {
+                mail: "user2@example.com",
+                name: "user2",
+                lastName: null,
+                university: {
+                  name: "UTN",
+                },
+              },
+            ],
+          });
+          expect(res.body).toHaveLength(1);
+        });
+    });
+    it("should get all projects that partially match their name and one of their users", async () => {
+      const name = "Manager";
+      const partialFullName = "User2";
+      await request(api)
+        .get(`/projects?name=${name}&user=${partialFullName}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.body[0]).toEqual({
+            name:
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            type: "Formal",
+            isDown: false,
+            department: {
+              name: "Ingenieria en Sistemas",
+            },
+            users: [
+              {
+                mail: "user1@example.com",
+                lastName: null,
+                name: "user1",
+                university: {
+                  name: "UTN",
+                },
+              },
+              {
+                mail: "user2@example.com",
+                name: "user2",
+                lastName: null,
+                university: {
+                  name: "UTN",
+                },
+              },
+            ],
+          });
+          expect(res.body).toHaveLength(1);
+        });
+    });
+  });
 });
