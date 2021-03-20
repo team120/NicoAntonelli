e03b15d7c43c662078076a62fe287d9754867665
ale <alejandrorecalde5@gmail.com>
2021-03-20 15:38:20 -0300

    Take into account sorting in project endpoint tests

--- e2e/project/project.get.api.test.ts
+++ e2e/project/project.get.api.test.ts
@@ -50,6 +50,68 @@ describe("Project actions", () => {
   });
 
   describe("search projects by a known property", () => {
+    describe("when isDown parameter is provided", () => {
+      describe("and set to false", () => {
+        it("should get every project", async () => {
+          const isDown = false;
+          await request(api)
+            .get(`/projects/?isDown=${isDown}`)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveLength(2);
+            });
+        });
+      });
+
+      describe("and set to true", () => {
+        it("should get no projects", async () => {
+          const isDown = true;
+          await request(api)
+            .get(`/projects/?isDown=${isDown}`)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveLength(0);
+            });
+        });
+        it("should get no projects as well when another parameter is provided", async () => {
+          const isDown = true;
+          const type = "Informal";
+          await request(api)
+            .get(`/projects/?isDown=${isDown}&type=${type}`)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveLength(0);
+            });
+        });
+      });
+    });
+    describe("dateFrom is sent", () => {
+      describe("less than a year", () => {
+        it("should get the UPM project only", async () => {
+          const dateFrom = new Date("2021-03-16");
+          dateFrom.setMonth(dateFrom.getMonth() - 8);
+          await request(api)
+            .get(`/projects?dateFrom=${dateFrom.toISOString()}`)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveLength(1);
+              expect(res.body[0].name).toBe("University Projects Manager");
+            });
+        });
+      });
+      describe("current date plus a month", () => {
+        it("should get no projects", async () => {
+          const dateFrom = new Date();
+          dateFrom.setMonth(dateFrom.getMonth() + 1);
+          await request(api)
+            .get(`/projects?dateFrom=${dateFrom}`)
+            .then((res) => {
+              expect(res.status).toBe(200);
+              expect(res.body).toHaveLength(0);
+            });
+        });
+      });
+    });
     describe("when project type is", () => {
       describe("Informal", () => {
         const projectName = "University Projects Manager";
@@ -79,80 +141,136 @@ describe("Project actions", () => {
         });
       });
     });
-    it("should get one project (UPM) that exactly match one of their users", async () => {
-      const userId = 3;
-      await request(api)
-        .get(`/projects?userId=${userId}`)
-        .then((res) => {
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveLength(1);
-          expect(res.body[0]).toEqual({
-            name: "University Projects Manager",
-            department: null,
-            type: "Informal",
-            isDown: false,
-            creationDate: "2021-03-16T17:13:02.000Z",
-            users: [
-              {
-                mail: "user2@example.com",
-                name: "Afak",
-                lastName: "Ename",
-                university: {
-                  id: 1,
-                  name: "UTN",
-                },
-              },
-              {
-                mail: "user3@example.com",
-                lastName: "Eaning",
-                name: "Nom",
-                university: {
-                  id: 1,
-                  name: "UTN",
-                },
-              },
-            ],
+  });
+
+  describe("sorting", () => {
+    describe("by project name", () => {
+      it("should get all projects sorted by name in ascending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=name&order=ascending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveLength(2);
+            expect(res.body[0].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+            expect(res.body[1].name).toBe("University Projects Manager");
           });
-        });
+      });
+
+      it("should get all projects sorted by name in descending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=name&order=descending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveLength(2);
+            expect(res.body[0].name).toBe("University Projects Manager");
+            expect(res.body[1].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+          });
+      });
+    });
+    describe("by project type", () => {
+      it("should get all projects in descending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=type&order=descending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveLength(2);
+            expect(res.body[0].name).toBe("University Projects Manager");
+            expect(res.body[1].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+          });
+      });
+      it("should get all projects in descending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=type&order=descending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveLength(2);
+            expect(res.body[0].name).toBe("University Projects Manager");
+            expect(res.body[1].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+          });
+      });
+    });
+    describe("by project creation date", () => {
+      it("should get all projects sorted by date in ascending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=creationDate&order=ascending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body[0].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+            expect(res.body[1].name).toBe("University Projects Manager");
+          });
+      });
+      it("should get all projects sorted by date in ascending order", async () => {
+        await request(api)
+          .get("/projects?sortBy=creationDate&order=ascending")
+          .then((res) => {
+            expect(res.status).toBe(200);
+            expect(res.body[0].name).toBe(
+              "Desarrollo de un sistema para identificar geoposicionamiento en entorno de Internet de la Cosas (IoT)",
+            );
+            expect(res.body[1].name).toBe("University Projects Manager");
+          });
+      });
     });
   });
-  describe("when isDown parameter is provided", () => {
-    describe("and set to false", () => {
-      it("should get every project", async () => {
-        const isDown = false;
+
+  it("should get one project (UPM) that exactly match one of their users", async () => {
+    const userId = 3;
+    await request(api)
+      .get(`/projects?userId=${userId}`)
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveLength(1);
+        expect(res.body[0]).toEqual({
+          name: "University Projects Manager",
+          department: null,
+          type: "Informal",
+          isDown: false,
+          creationDate: "2021-03-16T17:13:02.000Z",
+          users: [
+            {
+              mail: "user2@example.com",
+              name: "Afak",
+              lastName: "Ename",
+              university: {
+                id: 1,
+                name: "UTN",
+              },
+            },
+            {
+              mail: "user3@example.com",
+              lastName: "Eaning",
+              name: "Nom",
+              university: {
+                id: 1,
+                name: "UTN",
+              },
+            },
+          ],
+        });
+      });
+  });
+  describe("search projects by a general text search", () => {
+    describe("when exactly match some of their users", () => {
+      it("should get the two existent projects", async () => {
+        const generalSearchText = "fAk";
         await request(api)
-          .get(`/projects/?isDown=${isDown}`)
+          .get(`/projects?generalSearch=${generalSearchText}`)
           .then((res) => {
             expect(res.status).toBe(200);
             expect(res.body).toHaveLength(2);
           });
       });
     });
-
-    describe("and set to true", () => {
-      it("should get no projects", async () => {
-        const isDown = true;
-        await request(api)
-          .get(`/projects/?isDown=${isDown}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(0);
-          });
-      });
-      it("should get no projects as well when another parameter is provided", async () => {
-        const isDown = true;
-        const type = "Informal";
-        await request(api)
-          .get(`/projects/?isDown=${isDown}&type=${type}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(0);
-          });
-      });
-    });
-  });
-
-  describe("search projects by a general text search", () => {
     describe("when project name is partially matched", () => {
       it("should get all projects", async () => {
         const generalSearchText = "Manager";
@@ -246,46 +364,6 @@ describe("Project actions", () => {
               });
           });
         });
-      });
-    });
-  });
-
-  describe("when exactly match some of their users", () => {
-    it("should get the two existent projects", async () => {
-      const generalSearchText = "fAk";
-      await request(api)
-        .get(`/projects?generalSearch=${generalSearchText}`)
-        .then((res) => {
-          expect(res.status).toBe(200);
-          expect(res.body).toHaveLength(2);
-        });
-    });
-  });
-
-  describe("When only dateFrom is sent", () => {
-    describe("less than a year", () => {
-      it("should get the UPM project only", async () => {
-        const dateFrom = new Date("2021-03-16");
-        dateFrom.setMonth(dateFrom.getMonth() - 8);
-        await request(api)
-          .get(`/projects?dateFrom=${dateFrom.toISOString()}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0].name).toBe("University Projects Manager");
-          });
-      });
-    });
-    describe("current date plus a month", () => {
-      it("should get no projects", async () => {
-        const dateFrom = new Date();
-        dateFrom.setMonth(dateFrom.getMonth() + 1);
-        await request(api)
-          .get(`/projects?dateFrom=${dateFrom}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(0);
-          });
       });
     });
   });
