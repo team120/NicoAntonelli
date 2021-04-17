6ef11b30025fe01f07bee3e82915d321a2162ec5
Joshua Acciarri <acciarrijoshua@gmail.com>
2021-04-17 19:59:24 -0300

    E2E Default-Role api Updated

--- e2e/default-role/default-role.api.test.ts
+++ e2e/default-role/default-role.api.test.ts
@@ -7,7 +7,6 @@ setupCreateAndTeardownTestDb();
 describe("Default Roles actions", () => {
   describe("get default roles", () => {
     it("should return both formal and informal default roles", async () => {
-      const inResearchPack = false;
       await request(api)
         .get("/default-roles")
         .then((res) => {
@@ -19,9 +18,9 @@ describe("Default Roles actions", () => {
   });
   describe("get default roles with inResearchPack set to false", () => {
     it("should return only roles which are for Informal Projects", async () => {
-      const type = "informal";
+      const inResearchPack = false;
       await request(api)
-        .get(`/default-roles?inResearchPack=${type}`)
+        .get(`/default-roles?inResearchPack=${inResearchPack}`)
         .then((res) => {
           expect(res.status).toBe(200);
           expect(res.body).toHaveLength(2);
@@ -32,13 +31,40 @@ describe("Default Roles actions", () => {
   });
   describe("get default roles with inResearchPack set to true", () => {
     it("should return only roles which are for Formal Projects", async () => {
-      const type = "formal";
+      const inResearchPack = true;
       await request(api)
-        .get(`/default-roles?inResearchPack=${type}`)
+        .get(`/default-roles?inResearchPack=${inResearchPack}`)
         .then((res) => {
           expect(res.status).toBe(200);
           expect(res.body).toHaveLength(1);
-          expect(res.body[0].name).toBe("Director")
+          expect(res.body[0].name).toBe("Director");
+        });
+    });
+  });
+  describe("get one default role", () => {
+    it("should return default role with specified ID with asociated grants", async () => {
+      const id = 1;
+      await request(api)
+        .get(`/default-roles/${id}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+          expect(res.body.name).toBe("Member");
+          expect(res.body.inResearchPack).toBe(false);
+          expect(res.body.grants).toBeDefined();
+          expect(res.body.grants).toHaveLength(2);
+        });
+    });
+    it("should return appropiate message if ID is not found on DB", async () => {
+      const id = 100;
+      const message = `Item ${id} not found`;
+      await request(api)
+        .get(`/default-roles/${id}`)
+        .then((res) => {
+          expect(res.status).toBe(404);
+          expect(res.body.message).toBe(message);
+          expect(res.body.name).not.toBeDefined();
+          expect(res.body.description).not.toBeDefined();
+          expect(res.body.grants).not.toBeDefined();
         });
     });
   });
